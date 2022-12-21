d1c16630ddcfa7ba976b9b6175d2445ba4e6931a
yumahonda <115617624+yumahonda@users.noreply.github.com>
2022-12-21 15:18:29 +0900

    pro

--- kosakaq_provider.py
+++ kosakaq_provider.py
@@ -6,14 +6,12 @@ Created on Thu Nov 17 15:00:00 2022
 """
 
 import requests
-import warnings
 from qiskit.providers import ProviderV1 as Provider #抽象クラスのインポート
 from qiskit.providers.exceptions import QiskitBackendNotFoundError #エラー用のクラスをインポート
-from qiskit.providers.jobstatus import JobStatus
-from .exceptions import KosakaQTokenError, KosakaQBackendJobIdError #エラー用のクラス（自作）をインポート
+from .exceptions import KosakaQTokenError, KosakaQBackendJobIdError, KosakaQBackendValueError #エラー用のクラス（自作）をインポート
 from .kosakaq_backend import KosakaQBackend 
-from typing import Optional, Union, List, Dict, Any
-from datetime import datetime as python_datetime
+from .kosakaq_job import KosakaQJob
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 
 class KosakaQProvider(Provider): #抽象クラスからの継承としてproviderクラスを作る
 
@@ -23,9 +21,33 @@ class KosakaQProvider(Provider): #抽象クラスからの継承としてprovide
         self.name = 'kosakaq_provider' #nameという変数を右辺に初期化、このproviderクラスの名づけ
         self.url = 'http://192.168.11.85' #リンク変更可能
         self.wjson = '/api/backends.json' #jsonに何を入れてサーバーに送るか
+        self.jobson = '/job/'
     
-
-  
+    
+    def backends(self, name=None, **kwargs):#API(サーバー)に今使えるbackendを聞く(Rabiが使えるとかunicornが使えるとかをreturn[使えるもの]で教えてくれる)
+        """指定したフィルタリングと合うバックエンドたちを返すメソッド
+        引数:
+            name (str): バックエンドの名前(Rabiやunicorn).
+            **kwargs: フィルタリングに使用される辞書型
+        戻り値:
+            list[Backend]:　フィルタリング基準に合うバックエンドたちのリスト
+        """
+        self._backend = [] #availableなバックエンドクラスのbkednameを入れていくためのリスト
+        res = requests.get(self.url + self.wjson, headers={"Authorization": "Token " + self.access_token})
+        response = res.json() #[{'id': 1, 'bkedid': 0, 'bkedname': 'Rabi', 'bkedstatus': 'unavailable','detail': 'Authentication credentials were not provided',...}, {'id': 2, 'bkedid': 1, 'bkedname': 'Unicorn', 'bkedstatus': 'available'}]
+        if 'detail' in response[0]: #トークンが違ったらdetailの辞書一つだけがresponseのリストに入っていることになる
+            raise KosakaQTokenError('access_token was wrong') #トークン間違いを警告
+        for i in range(len(response)):
+            if response[i]['bkedstatus'] =='available':
+                if name == None:
+                    self._backend.append(KosakaQBackend(self, response[i]['bkedname'], self.url, response[i]['bkedversion'], response[i]['bkednqubits'], 4096, 1))
+                elif name == response[i]['bkedname']:
+                    self._backend.append(KosakaQBackend(self, response[i]['bkedname'], self.url, response[i]['bkedversion'], response[i]['bkednqubits'], 4096, 1))  
+                else:
+                    pass
+        return self._backend#responseのstatusがavailableかつフィルタリングにあうバックエンドたちのバックエンドクラスのインスタンスリストを返す
+ 
+    
     def get_backend(self, name=None, **kwargs): #ユーザーに"Rabi"などを引数として入れてもらう、もしbackendsメソッドのreturnにRabiがあればインスタンスを作れる
         """指定されたフィルタリングに合うバックエンドを一つだけ返す(一つ下のメソッドbackendsの一つ目を取り出す)
        引数:
@@ -43,86 +65,8 @@ class KosakaQProvider(Provider): #抽象クラスからの継承としてprovide
             raise QiskitBackendNotFoundError('No backend matches criteria.')
 
         return backends[0]
-    
-    def backends(self, name=None, **kwargs):#API(サーバー)に今使えるbackendを聞く(Rabiが使えるとかunicornが使えるとかをreturn[使えるもの]で教えてくれる)
-        """指定したフィルタリングと合うバックエンドたちを返すメソッド
-        引数:
-            name (str): バックエンドの名前(Rabiやunicorn).
-            **kwargs: フィルタリングに使用される辞書型
-        戻り値:
-            list[Backend]:　フィルタリング基準に合うバックエンドたちのリスト
-        """
-        self._backend=[] #availableなバックエンドクラスのbkednameを入れていくためのリスト
-        res = requests.get(self.url + self.wjson, headers={"Authorization": "Token " + self.access_token})
-        response = res.json() #[{'id': 1, 'bkedid': 0, 'bkedname': 'Rabi', 'bkedstatus': 'unavailable','detail': 'Authentication credentials were not provided',...}, {'id': 2, 'bkedid': 1, 'bkedname': 'Unicorn', 'bkedstatus': 'available'}]
-        if 'detail' in response[0]: #トークンが違ったらdetailの辞書一つだけがresponseのリストに入っていることになる
-            raise KosakaQTokenError('access_token was wrong') #トークン間違いを警告
-        for i in range(len(response)):
-            if response[i]['bkedstatus'] =='available':
-                if name == None:
-                    self._backend.append(KosakaQBackend(self, response[i]['bkedname'], self.url, response[i]['bkedversion'], response[i]['bkednqubits'], 4096, 1))
-                elif name == response[i]['bkedname']:
-                    self._backend.append(KosakaQBackend(self, response[i]['bkedname'], self.url, response[i]['bkedversion'], response[i]['bkednqubits'], 4096, 1)  )  
-                else:
-                    pass
-        return self._backend#responseのstatusがavailableかつフィルタリングにあうバックエンドたちのバックエンドクラスのインスタンスリストを返す
-    
-    #過去に行ったjobをサーバーから条件指定して取り出す、jobのリストを返す
-    def jobs(self,
-            limit: int = 10,
-            skip: int = 0,
-            status: Optional[Union[JobStatus, str, List[Union[JobStatus, str]]]] = None,
-            job_name: Optional[str] = None,
-            start_datetime: Optional[python_datetime] = None,
-            end_datetime: Optional[python_datetime] = None,
-            job_tags: Optional[List[str]] = None,
-            job_tags_operator: Optional[str] = "OR",
-            experiment_id: Optional[str] = None,
-            descending: bool = True,
-            db_filter: Optional[Dict[str, Any]] = None
-    ): #list[KosakaQJob]を返す
-        """このバックエンドに送信されたjobのうち指定したフィルタに合うものを取得して、必要があればページ分割する。
-        サーバーは一回のコールで返すjobの数に制限があるのでサーバーを何度も呼び出すかもしれない。ページ分割をより細かく制御するにはskipパラメータを参照。
-        引数:
-            limit: 取得するjobの数
-            skip: job取得の検索を開始するインデックス.
-            status: このステータスを持つjobのみを取得する。
-                    指定方法の例 `status=JobStatus.RUNNING` or `status="RUNNING"`
-                    or `status=["RUNNING", "ERROR"]`
-            job_name: jobの名前でのフィルタリング用。 job名は部分的にマッチする、そして
-                    `regular expressions(正規表現)
-               <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions>`_
-               が使われる。
-            start_datetime: 指定された開始日（現地時間）でのフィルター用。これは作成日がこの指定した現地時間より後のjobを探すのに使われる。
-            end_datetime: 指定された終了日（現地時間）でのフィルター用。 これは作成日がこの指定された現地時間よりも前に作られたjobを見つけるために使われる。
-            job_tags: jobに割り当てられたタグでフィルターするための引数。
-            job_tags_operator: job_tagsでフィルタリングする際に使用する論理演算子。"AND"か"OR"が有効な値。
-                   *もしANDを指定した場合、jobにはjob_tagsで指定されたすべてのタグが必要。
-                   *もしORを指定した場合、jobはjob_tagsで指定されたタグのいずれかを持っている必要がある。
-            experiment_id: jobのexperimentIDでフィルタリング用。
-            descending:もし"True"ならjobの作成日の降順で(新しいものから)jobを返す。
-                      "False"なら昇順で（古いものから）jobを返す。
-            db_filter: ループバックベースのフィルタ
-               <https://loopback.io/doc/en/lb2/Querying-data.html>`_.
-               これはデータベースの"where"フィルターへのインターフェースである。
-               使用例
-               エラーのある直近の5つのjobをフィルタリング::
-                  job_list = backend.jobs(limit=5, status=JobStatus.ERROR)
-               ハブ名"kosaka-q(ibm-q)"を持つ5つのjobをフィルタリング::
-                 filter = {'hubInfo.hub.name': 'kosaka-q(ibm-q)'}
-                 job_list = backend.jobs(limit=5, db_filter=filter)
-        戻り値:
-           条件に合うjobのリスト
-        Raises:
-           KosakaQBackendValueError: キーワード値が認識されない場合 (でも、メソッド内で使われてない)
-        """
-        return self.jobs( #このjobは違う
-            limit=limit, skip=skip, backend_name=self.name(), status=status,
-            job_name=job_name, start_datetime=start_datetime, end_datetime=end_datetime,
-            job_tags=job_tags, job_tags_operator=job_tags_operator,
-            experiment_id=experiment_id, descending=descending, db_filter=db_filter)
-        
-   
+
+
     def retrieve_job(self, job_id: str):#jobidを文字列として代入してもらう（指定してもらう）,対応するJobを返す
         """このバックエンドに投入されたjobを一つだけ返す
         引数:
@@ -130,23 +74,139 @@ class KosakaQProvider(Provider): #抽象クラスからの継承としてprovide
         戻り値:
             与えられたIDのjob
         Raises:
-            KosakaQBackendJobIdError: もしjobの取得に失敗した場合
+            KosakaQBackendJobIdError: もしjobの取得に失敗した場合、ID間違いのせいにする
         """
-        job = self.retrieve_job(job_id) #このメソッドたぶん違う
-        job_backend = job.backend()
+        res = requests.get(self.url + self.jobson, headers={"Authorization": "Token " + self.access_token}, params={"jobid": self.job._job_id})
+        response = res.json()#辞書型{bkedlist,jobist,qobjlist}
+        if response['joblist']['jobid'] == job_id:
+            for i in range(len(response['bkedlist'])):
+                if response['bkedlist'][i]['bkedid'] == response['joblist']['bkedid']:
+                    bkedname = response['bkedlist'][i]['bkedname']
+            backend = KosakaQBackend(self, bkedname, self.url, response['bkedlist'][i]['bkedversion'], response['bkedlist'][i]['bkednqubits'], 4096, 1)  
+            #量子レジスタqを生成する。
+            q = QuantumRegister(1)
+            #古典レジスタcを生成する
+            c = ClassicalRegister(2)
+            qc = QuantumCircuit(q, c)
+            string = response['qobjlist']['gates']
+            gateslist = eval(string) #gatelist=["H","X"]
+            for gate_name in gateslist:
+                if gate_name == "I":
+                    qc.i(q[0])
+                elif gate_name == "X":
+                    qc.x(q[0])
+                elif gate_name == "Y":
+                    qc.y(q[0])
+                elif gate_name == "Z":
+                    qc.z(q[0])
+                elif gate_name == "H":
+                    qc.h(q[0])
+                elif gate_name == "S":
+                    qc.s(q[0])
+                elif gate_name == "SDG":
+                    qc.sdg(q[0])
+                elif gate_name == "SX":
+                    qc.sx(q[0])
+                elif gate_name == "SXDG":
+                    qc.sxdg(q[0])
+                else:
+                    pass
+                
+            return KosakaQJob(backend, response['joblist']['jobid'], self.access_token, qc)
+        else:
+            raise KosakaQBackendJobIdError('Job_id was wrong')
+     
 
-        if self.name() != job_backend.name():
-            warnings.warn('Job {} belongs to another backend than the one queried. '
-                          'The query was made on backend {}, '
-                          'but the job actually belongs to backend {}.'
-                          .format(job_id, self.name(), job_backend.name()))
-            raise KosakaQBackendJobIdError('Failed to get job {}: '
-                                   'job does not belong to backend {}.'
-                                   .format(job_id, self.name()))
-
-        return job
-
+    def jobs(self,
+            limit: int = 10,
+            skip: int = 0,
+            jobstatus = None,
+            jobid = None,
+            begtime = None,
+            bkedid = None,
+            fintime = None,
+            job_num = None,
+            note = None,
+            posttime = None,
+            qobjid = None,
+            userid = None
+    ): #list[KosakaQJob]を返す
+        """このバックエンドに送信されたjobのうち指定したフィルタに合うものを取得して、返す
+        引数:
+            limit: 取得するjobの上限数
+            skip: jobを飛ばしながら参照
+            jobstatus: このステータスを持つjobのみを取得する。
+                    指定方法の例 `status=JobStatus.RUNNING` or `status="RUNNING"`
+                    or `status=["RUNNING", "ERROR"]`
+            jobid: jobの名前でのフィルタリング用。 job名は部分的にマッチする、そして
+                    `regular expressions(正規表現)
+               <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions>`_
+               が使われる。
+            begtime: 指定された開始日（現地時間）でのフィルター用。これは作成日がこの指定した現地時間より後のjobを探すのに使われる。
+            fintime: 指定された終了日（現地時間）でのフィルター用。 これは作成日がこの指定された現地時間よりも前に作られたjobを見つけるために使われる。
+            job_num: jobの番号
+            note: メモ
+            posttime: 投稿時間
+            qobjid: qobjのID
+            userid: ユーザーID
+  
+        戻り値:
+           条件に合うjobのリスト
+        Raises:
+           KosakaQBackendValueError: キーワード値が認識されない場合 (でも、メソッド内で使われてない)
+        """
+        jobs_list = [] #返す用のリスト
+        res = requests.get(self.url + self.jobson, headers={"Authorization": "Token " + self.access_token})
+        response = res.json()#辞書型{bkedlist(ラビ、ユニコーン),jobist(jobnumがjobの数だけ),qobjlist(qobjnumがqobjの数だけ、ゲートもたくさん)}
         
+        for i in range(len(response['joblist'])):
+            if response['joblist'][i][0] == begtime:
+                if response['joblist'][i][1] == bkedid:
+                    if response['joblist'][i][2] == fintime:
+                        if response['joblist'][i][4] == job_num:
+                            if response['joblist'][i][5] == jobid:
+                                if response['joblist'][i][6] == jobstatus:
+                                    if response['joblist'][i][7] == note:
+                                        if response['joblist'][i][8] == posttime:
+                                            if response['joblist'][i][9] == qobjid:
+                                                if response['joblist'][i][10] == userid:
+                                                    bked_id = response['joblist'][i][1]#代入用bkedid
+                                                    bked_posi =bked_id-1 #代入用バックエンド番号
+                                                    backend = KosakaQBackend(self, response['bkedlist'][bked_posi]['bkedname'], self.url, response['bkedlist'][bked_posi]['bkedversion'], response['bkedlist'][bked_posi]['bkednqubits'], 4096, 1)
+                                                    #量子レジスタqを生成する。
+                                                    q = QuantumRegister(1)
+                                                    #古典レジスタcを生成する
+                                                    c = ClassicalRegister(2)
+                                                    qc = QuantumCircuit(q, c)
+                                                    string = response['qobjlist'][i]['gates']
+                                                    gateslist = eval(string) #gatelist=["H","X"]
+                                                    for gate_name in gateslist:
+                                                        if gate_name == "I":
+                                                            qc.i(q[0])
+                                                        elif gate_name == "X":
+                                                            qc.x(q[0])
+                                                        elif gate_name == "Y":
+                                                            qc.y(q[0])
+                                                        elif gate_name == "Z":
+                                                            qc.z(q[0])
+                                                        elif gate_name == "H":
+                                                            qc.h(q[0])
+                                                        elif gate_name == "S":
+                                                            qc.s(q[0])
+                                                        elif gate_name == "SDG":
+                                                            qc.sdg(q[0])
+                                                        elif gate_name == "SX":
+                                                            qc.sx(q[0])
+                                                        elif gate_name == "SXDG":
+                                                            qc.sxdg(q[0])
+                                                        else:
+                                                            pass
+                                                    jobs_list.insert(i, KosakaQJob(backend, response['joblist'][i]['jobid'], self.access_token, qc))
+        if len(jobs_list) == 0:
+            raise KosakaQBackendValueError('key word was wrong')
+        return jobs_list
+                
+ 
     def __eq__(self, other): #等号の定義
         """Equality comparison.
         By default, it is assumed that two `Providers` from the same class are
